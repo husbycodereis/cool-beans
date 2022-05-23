97ee599df0dec937b6d845e98a16e361d5b5cd65
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-05-23 13:12:25 +0300

    Initial Commit

---
+++ Shared/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  Shared
+//
+//  Created by Ali Riza Reisoglu on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
