118a4044b815b3ddddcc3db744a0ec12e7ef7904
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-05-23 17:01:23 +0300

    created the calorie / caffeine calculation logic

--- Shared/CustomizeView.swift
+++ Shared/CustomizeView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+enum SizeOptions: String, CaseIterable{
+    case Small, Medium, Large
+}
+
 struct CustomizeView: View {
     let drink: Drink
     
@@ -16,25 +20,66 @@ struct CustomizeView: View {
     @State private var extraShots = 0
     @State private var milk = ConfigurationOption.none
     @State private var syrup = ConfigurationOption.none
-    let sizeOptions = ["Small", "Medium", "Large"]
+    let sizeOptions = SizeOptions.allCases
     
     var caffeine: Int {
-        100
+        var caffeineAmount = drink.caffeine[size]
+        caffeineAmount += (extraShots * 60)
+        
+        if isDecaf {
+            caffeineAmount /= 20
+        }
+        return caffeineAmount
     }
     
     var calories: Int {
-        100
+        var calorieAmount = drink.baseCalories
+        calorieAmount += extraShots * 10
+        
+        if drink.coffeeBased {
+            calorieAmount += milk.calories
+        } else {
+            calorieAmount += milk.calories / 8
+        }
+        
+        calorieAmount += syrup.calories
+        return calorieAmount * (size + 1)
     }
+    
+    
     var body: some View {
         Form {
             Section("Basic options") {
                 Picker("Size", selection: $size) {
-                    ForEach(sizeOptions.indices) {index in
-                        Text(sizeOptions[index])
+                    //it can also be written as ForEach(sizeOptions.indices, id: \.self)
+                    //When using ForEach in #SwiftUI with a non-constant range, remember to pass it an id.
+                    ForEach(0 ..< sizeOptions.count, id: \.self) {index in
+                        Text(sizeOptions[index].rawValue)
                     }
                 }
                 .pickerStyle(.segmented)
+                if drink.coffeeBased {
+                    Stepper("Extra shots: \(extraShots)",
+                value: $extraShots, in: 0...8)
+                }
                 Toggle("Decaffeinated", isOn: $isDecaf)
+            }
+            Section("Customizations"){
+                Picker("Milk", selection: $milk){
+                    ForEach(menu.milkOptions){option in
+                        Text(option.name)
+                            .tag(option)
+                    }
+                }
+            }
+            
+            if drink.coffeeBased {
+                Picker("Syrup", selection: $syrup) {
+                    ForEach(menu.syrupOptions) {option in
+                        Text(option.name)
+                            .tag(option)
+                    }
+                }
             }
             Section("Estimates") {
                 Text("Caffeine: \(caffeine)mg")
---
+++ Shared/Serving.swift
@@ -0,0 +1,8 @@
+//
+//  Serving.swift
+//  CoolBeans (iOS)
+//
+//  Created by Ali Riza Reisoglu on 23.05.2022.
+//
+
+import Foundation
